0c6640ccaa03b33573ce58ff6e08494a4ab50dec
NadaElsayed86 <158353202+NadaElsayed86@users.noreply.github.com>
2024-02-06 19:51:10 +0200

    Add files via upload

---
+++ Week1/task4..c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x=7;
+    int y=4;
+
+    printf("x|y=%d\n",x|y);
+    printf("x&y=%d\n",x&y);
+    printf("x^y=%d\n",x^y);
+    printf("x<<1=%d\n",x<<1);
+    printf("x>>2=%d",x>>2);
+    return 0;
+}
